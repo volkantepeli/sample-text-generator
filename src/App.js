9f5cdcd594bc4eeb6465157bbf87aeef7fe018d4
volkantepeli <volkan.tepelii@gmail.com>
2022-09-28 20:22:13 +0300

    the project is complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css" ;
+import React, { useState , useEffect} from 'react'
+import Options from "./components/Options";
+import Output from "./components/Output";
 
 function App() {
+  const [paragraphs, setParagraphs] = useState([]);
+  const [tag, setTag] = useState ('p');
+  const [inputValue, setInputValue] = useState(4);
+  const [includeHtml, setIncludeHtml] = useState ("Yes");
+
+  useEffect(() => {
+    const url = `https://baconipsum.com/api/?type=all-meat&paras=${inputValue}&start-with-lorem=1` ;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setParagraphs(data));
+  },[inputValue])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="title">
+            <h1>React sample text generator app</h1>
+        </div>
+        <Options 
+        paragraphs={paragraphs} 
+        includeHtml={includeHtml} 
+        setIncludeHtml={setIncludeHtml}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        tag={tag}
+        setTag={setTag}
+        />
+        <Output paragraphs={paragraphs} tag={tag} includeHtml={includeHtml} />
+      </div>
     </div>
   );
 }
